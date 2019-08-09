1c6b3d99c6006ba8fbf7f61b2fb5daeda5e30c82
Redvan <redvanisation@gmail.com>
2019-08-09 11:13:57 -0400

    fixing the users delete issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
-
 end
--- test/controllers/application_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class ApplicationControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
